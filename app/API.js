7a5f83eac28a128f9fa71781bcd1f4b7f413f87f
Amirreza <amir.nf713@gmail.com>
2025-04-18 23:40:56 +0330

    modified:   app/API.js

--- app/API.js
+++ app/API.js
@@ -1,13 +1,12 @@
 
 const Url = 'https://janebi-speed.ir' //  'https://janebi-speed.ir'
 const apiKey = ({
-
     sendSms: `${Url}/api/register/sms/smsSend`,
     postUser: `${Url}/api/register/user/`,
-    getOneUser: `${Url}/api/register/user/`,  // :num
-    getOneUserid: `${Url}/api/register/user/ida`,  // :id
-    getOneAdmin: `${Url}/api/admin/`,  // :num
-    postCategori: `${Url}/api/categori/`, 
+    getOneUser: `${Url}/api/register/user/`,
+    getOneUserid: `${Url}/api/register/user/ida`,
+    getOneAdmin: `${Url}/api/admin/`,
+    postCategori: `${Url}/api/categori/`, // مسیر 'categori' ممکنه اشتباه باشه
     getCategori: `${Url}/api/categori/`,
     deleteCategori: `${Url}/api/categori/`,
     postitem: `${Url}/api/Kala/`,
@@ -15,26 +14,16 @@ const apiKey = ({
     getNewitem: `${Url}/api/Kala/new/`,
     getOffitem: `${Url}/api/Kala/off/`,
     getoneitem: `${Url}/api/Kala/`,
-    updateItem: `${Url}/api/Kala/`, // :id
-    deleteItem: `${Url}/api/Kala/`, // :id
-    getStayincall: `${Url}/api/stayincall/`, 
-    bascket: `${Url}/api/basket/`, 
-    Web: `${Url}/api/webdata/`, 
-    postStayincall: `${Url}/api/stayincall`, 
-    Offer: `${Url}/api/Offercode`, 
-    gif: `${Url}/api/Gitcode`, 
-    user: `${Url}/api/register/user`, 
-    
+    updateItem: `${Url}/api/Kala/`,
+    deleteItem: `${Url}/api/Kala/`,
+    getStayincall: `${Url}/api/stayincall/`,
+    bascket: `${Url}/api/basket/`,
+    Web: `${Url}/api/webdata/`,
+    postStayincall: `${Url}/api/stayincall`,
+    Offer: `${Url}/api/Offercode`,
+    gif: `${Url}/api/Gitcode`,
+    user: `${Url}/api/register/user`,
+});
 
-
-
-
-
-
-
-
-
-
-})
 
 export default apiKey
