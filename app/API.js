718a20350fcebcebfcea489c0b83049e0b6ff4e8
Amirreza <amir.nf713@gmail.com>
2025-04-18 03:53:22 +0330

    modified:   app/API.js

--- app/API.js
+++ app/API.js
@@ -1,5 +1,5 @@
 
-const Url = 'http://localhost:3001' //  'https://janebi-speed.ir'
+const Url = 'https://janebi-speed.ir' //  'https://janebi-speed.ir'
 const apiKey = ({
 
     sendSms: `${Url}/api/register/sms/smsSend`,
