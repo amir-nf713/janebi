2d04fc397607195bc8c116fa27f64624e28b44a2
Amirreza <amir.nf713@gmail.com>
2025-04-18 04:41:27 +0330

    modified:   app/API.js

--- app/API.js
+++ app/API.js
@@ -1,41 +1,26 @@
-
-const Url = 'https://janebi-speed.ir' //  'https://janebi-speed.ir'
-const apiKey = ({
-
+const apiKey = {
     sendSms: `${Url}/api/register/sms/smsSend`,
-    postUser: `${Url}/api/register/user/`,
-    getOneUser: `${Url}/api/register/user/`,  // :num
-    getOneUserid: `${Url}/api/register/user/ida`,  // :id
-    getOneAdmin: `${Url}/api/admin/`,  // :num
-    postCategori: `${Url}/api/categori/`, 
-    getCategori: `${Url}/api/categori/`,
-    deleteCategori: `${Url}/api/categori/`,
-    postitem: `${Url}/api/Kala/`,
-    getitem: `${Url}/api/Kala/`,
-    getNewitem: `${Url}/api/Kala/new/`,
-    getOffitem: `${Url}/api/Kala/off/`,
-    getoneitem: `${Url}/api/Kala/`,
-    updateItem: `${Url}/api/Kala/`, // :id
-    deleteItem: `${Url}/api/Kala/`, // :id
-    getStayincall: `${Url}/api/stayincall/`, 
-    bascket: `${Url}/api/basket/`, 
-    Web: `${Url}/api/webdata/`, 
+    postUser: `${Url}/api/register/user`,
+    getOneUser: `${Url}/api/register/user`,
+    getOneUserid: `${Url}/api/register/user/id`,
+    getOneAdmin: `${Url}/api/admin`,
+    postCategori: `${Url}/api/categori`, 
+    getCategori: `${Url}/api/categori`,
+    deleteCategori: `${Url}/api/categori`,
+    postitem: `${Url}/api/Kala`,
+    getitem: `${Url}/api/Kala`,
+    getNewitem: `${Url}/api/Kala/new`,
+    getOffitem: `${Url}/api/Kala/off`,
+    getoneitem: `${Url}/api/Kala`,
+    updateItem: `${Url}/api/Kala`,
+    deleteItem: `${Url}/api/Kala`,
+    getStayincall: `${Url}/api/stayincall`, 
+    basket: `${Url}/api/basket`, 
+    Web: `${Url}/api/webdata`, 
     postStayincall: `${Url}/api/stayincall`, 
     Offer: `${Url}/api/Offercode`, 
     gif: `${Url}/api/Gitcode`, 
-    user: `${Url}/api/register/user`, 
-   
-    
+    user: `${Url}/api/register/user`
+}
 
-
-
-
-
-
-
-
-
-
-})
-
-export default apiKey
+module.exports = apiKey; // اگر از require استفاده می‌کنید
