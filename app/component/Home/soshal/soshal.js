8bba66bb732985dccba719fe6f8ede9488fda418
Amirreza <amir.nf713@gmail.com>
2025-04-23 19:32:38 +0330

    modified:   app/component/Home/soshal/soshal.js

--- app/component/Home/soshal/soshal.js
+++ app/component/Home/soshal/soshal.js
@@ -10,40 +10,50 @@ import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Soshal() {
 
-    const [Basket, setBasket] = useState([]);
+    // const [Basket, setBasket] = useState([]);
+    const [ins, setins] = useState([]);
+    const [wat, setwat] = useState([]);
+    const [tel, settel] = useState([]);
 
     useEffect(() => {
-      
-        axios.get(apiKey.Web) // اضافه کردن `/` برای درستی URL
-          .then(response => {
-            if (response.data.data) {
-              setBasket([response.data.data]); // تبدیل شیء به آرایه برای جلوگیری از خطا
-            }
-          })
-          .catch(error => console.error("Error fetching data: ",error));
-      
-    }, []); // وابستگی به `id`
+      const fetchShippingCost = async () => {
+        try {
+          const response = await axios.get(apiKey.Web);
+          setins(response?.data?.data?.[0]?.instagram || "");
+          setwat(response?.data?.data?.[0]?.wahtsapp || "");
+          settel(response?.data?.data?.[0]?.telegram || "");
+        } catch (err) {
+          console.error("Failed to fetch shipping cost:", err);
+          setShippingCost(0);
+        }
+      };
+  
+      fetchShippingCost();
+    }, []);
+    
+
+    
   return (
     <div className='w-full'>
-        {
-            Basket.map((item, index) => {
-             return(
+        
+            
+             
                 <div key={index} className="text-5xl flex max-mobile-l:text-3xl justify-center items-center w-full h-24 bg-sky-800">
-                  <Link className='mx-6 text-white ' href={`${item.instagram}`}>
+                  <Link className='mx-6 text-white ' href={ins}>
                      <img src="ico/67c7195642931.png" className='size-20 max-Wide-mobile-s:size-16 max-mobile-l:size-12 ' alt="img" />
                   </Link>
 
-                  <Link className='mx-6 text-white ' href={`${item.telegram}`}>
+                  <Link className='mx-6 text-white ' href={tel}>
                       <img src="ico/67c719811ab3e.png" className='size-20 max-Wide-mobile-s:size-16 max-mobile-l:size-12 ' alt="img" />
                   </Link>
 
-                  <Link className='mx-6 text-white ' href={`${item.wahtsapp}`}>
+                  <Link className='mx-6 text-white ' href={wat}>
                       <img src="ico/67c719c6f2533.png" className='size-20 max-Wide-mobile-s:size-16 max-mobile-l:size-12 ' alt="img" />
                   </Link>
                 </div>
-             )
-            })
-        }
+             
+            
+        
       
     </div>
   )
